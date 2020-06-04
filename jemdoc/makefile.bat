204e0f0d02cbb06085c81a382c5fe1c273651b1e
xuxinzi <xu_xinzi@163.com>
2020-06-04 20:17:01 +0800

    update webpage

--- jemdoc/makefile.bat
+++ jemdoc/makefile.bat
@@ -5,6 +5,6 @@ python ./jemdoc.py -o ../ teaching.jemdoc
 python ./jemdoc.py -o ../ services.jemdoc
 python ./jemdoc.py -o ../ awards.jemdoc
 python ./jemdoc.py -o ../ contact.jemdoc
-python ../ref/bibconvert.py --suffix web --highlight "Y. Li" --highlight "Yongfu Li" --highlight "Li, Yongfu" --input ../ref/Top.bib --input ../ref/publications.bib --header publications_header.jemdoc > publications.jemdoc
+python ../ref/bibconvert.py --suffix web --highlight "X.\ Xu" --highlight "Xinzi\ Xu" --highlight "Xu,\ Xinzi" --input ../ref/Top.bib --input ../ref/publications.bib --header publications_header.jemdoc > publications.jemdoc
 python ./jemdoc.py  -o ../ publications.jemdoc
 pause
